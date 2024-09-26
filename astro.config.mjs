8fce0a2c4ea9c1f50033ddfc2cee7827be6dd0ef
Mustafa Abukhadra <abukhadra@gmail.com>
2024-09-26 23:26:22 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://abukhadra.github.io',
+    base: 'seen-site',    
     integrations: [
         starlight({
             // title: 'Seen',
